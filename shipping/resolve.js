1ec698c215c24b74c669cac4a8d0bbd9196dde0b
starshipedu <157521232+starshipedu@users.noreply.github.com>
2026-05-31 23:34:28 +0700

    Create resolve.js

---
+++ shipping/resolve.js
@@ -0,0 +1,22 @@
+{
+  "success": true,
+  "metadata": {
+    "version": "5.0",
+    "initialized": true,
+    "loadedAt": 1704038400000,
+    "lastReloadAt": 1704038400000,
+    "githubBaseUrl": "https://raw.githubusercontent.com/...",
+    "config": {
+      "safetyMargin": 1.15,
+      "roundToNearest": 500,
+      "minimumShippingCost": 10000,
+      "debugMode": false
+    },
+    "stats": {
+      "zones": 50,
+      "cities": 514,
+      "products": 45,
+      "couriers": 10
+    }
+  }
+}
